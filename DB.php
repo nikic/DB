f18b8f18a7ae25f8068952aeaaf69274b0272679
nikic <privat@ni-po.com>
2010-06-20 17:04:16 +0200

    initial commit

---
+++ DB.php
@@ -0,0 +1,85 @@
+<?php
+	class DB
+	{
+		private static $instance = null;
+		
+		final private function __construct() {}
+		final private function __clone() {}
+		
+		private static function instance() {
+			if (self::$instance === null) {
+				self::$instance = new PDO(
+					'mysql:host='.DB_HOST.';dbname='.DB_NAME,
+					DB_USER,
+					DB_PASS,
+					array(
+						PDO::ATTR_PERSISTENT => true,
+						PDO::MYSQL_ATTR_INIT_COMMAND => 'SET CHARACTER SET utf8',
+					)
+				);
+				self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+			
+			return self::$instance;
+		}
+		
+		public static function __callStatic($method, $args) {
+			return call_user_func_array(array(self::instance(), $method), $arguments);
+		}
+		
+		public static function q() {
+			$args = func_get_args();
+			$c = count($args);
+			if ($c == 0) {
+				return self::instance()->query($query);
+			}
+			
+			return self::instance()->query(self::autoQuote($query, $c == 1 && is_array($args[0]) ? $args[0] : $args));
+		}
+		
+		public static function x($query) {
+			$args = func_get_args();
+			$c = count($args);
+			if ($c == 0) {
+				return self::instance()->exec($query);
+			}
+			
+			return self::instance()->exec(self::autoQuote($query, $c == 1 && is_array($args[0]) ? $args[0] : $args));
+		}
+		
+		public static function autoQuote($query, $args) {
+			$c = count($args); // num of args
+			$l = strlen($query); // length of query
+			$i = 0; // position in string
+			while (
+				   $c-- // there still are args
+				&& false !== ($i = strpos($query, '?', $i)) // needle still exists
+				&& $i < $l // not near end
+			) {
+				// $i+1 is the quote-r
+				if ($i+1 >= $l || false === $type = strpos('si', $query[$i+1])) {
+					// no or unsupported quote-r given
+					// => direct insert
+					$replace = array_shift($args);
+					$query = substr_replace($query, $replace, $i, 1);
+					$i += strlen($replace);
+					$l += strlen($replace);
+					continue;
+				}
+				
+				if ($type == 0) {
+					$replace = '\''.addslashes(array_shift($args)).'\'';
+				}
+				elseif ($type == 1) {
+					$replace = intval(array_shift($args));
+				}
+				
+				$query = substr_replace($query, $replace, $i, 2);
+				$i += strlen($replace)-1; // -1 due to
+				$l += strlen($replace)-1; // removal of quote-r
+			}
+			
+			return $query;
+		}
+	}
+?>
