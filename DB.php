483f981cecf93fa3f612270d2793f65f2afda350
nikic <+@ni-po.com>
2010-12-26 19:01:29 +0100

    annotate return values of instance() and q()
    This largely improves IDE hints

--- DB.php
+++ DB.php
@@ -2,10 +2,13 @@
     class DB
     {
         protected static $instance = null;
-        
+
         final private function __construct() {}
         final private function __clone() {}
-        
+
+        /**
+         * @return PDO
+         */
         public static function instance() {
             if (self::$instance === null) {
                 try {
@@ -23,39 +26,42 @@
                     die('Database connection could not be established.');
                 }
             }
-            
+
             return self::$instance;
         }
-        
+
+        /**
+         * @return PDOStatement
+         */
         public static function q($query) {
             if (func_num_args() == 1) {
                 return self::instance()->query($query);
             }
-            
+
             $args = func_get_args();
             return self::instance()->query(self::autoQuote(array_shift($args), $args));
         }
-        
+
         public static function x($query) {
             if (func_num_args() == 1) {
                 return self::instance()->exec($query);
             }
-            
+
             $args = func_get_args();
             return self::instance()->exec(self::autoQuote(array_shift($args), $args));
         }
-        
+
         public static function autoQuote($query, array $args) {
             $i = strlen($query);
             $c = count($args);
-            
+
             if ($c != substr_count($query, '?')) {
                 throw new UnexpectedValueException('Wrong parameter count: Number of placeholders and parameters does not match');
             }
-            
+
             while ($c--) {
                 while ($i-- && $query[$i] != '?');
-                
+
                 // $i+1 is the quote-r
                 if (!isset($query[$i+1]) || false === $type = strpos('sia', $query[$i+1])) {
                     // no or unsupported quote-r given
@@ -63,7 +69,7 @@
                     $query = substr_replace($query, $args[$c], $i, 1);
                     continue;
                 }
-                
+
                 if ($type == 0) {
                     $replace = self::instance()->quote($args[$c]);
                 } elseif ($type == 1) {
@@ -74,13 +80,13 @@
                     }
                     $replace = '(' . implode(',', $args[$c]) . ')';
                 }
-                
+
                 $query = substr_replace($query, $replace, $i, 2);
             }
-            
+
             return $query;
         }
-        
+
         public static function __callStatic($method, $args) {
             return call_user_func_array(array(self::instance(), $method), $args);
         }
