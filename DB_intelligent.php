d6f33f839500be1631b7e14ddaba0e7b00b46443
nikic <+@ni-po.com>
2011-03-11 18:49:57 +0100

    Drop PDO::ATTR_PERSISTENT and utf8 encoding switch from default constructor
    Reasoning:
    Persistent connections are considered harmful, because they may break things with transactions, locks, etc, without giving any performance improvement (normally even a degradation).
    Utf8 encoding should be specified in my.cnf, instead of being sent on every connection creation.

--- DB_intelligent.php
+++ DB_intelligent.php
@@ -6,23 +6,19 @@
             '&' => ' AND ',
             '|' => ' OR ',
         );
-        
+
         private static $instance = null;
-        
+
         final private function __construct() {}
         final private function __clone() {}
-        
+
         public static function instance() {
             if (self::$instance === null) {
                 try {
                     self::$instance = new PDO(
                         'mysql:host='.DB_HOST.';dbname='.DB_NAME,
                         DB_USER,
-                        DB_PASS,
-                        array(
-                            PDO::ATTR_PERSISTENT => true,
-                            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET CHARACTER SET utf8',
-                        )
+                        DB_PASS
                     );
                     self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 }
@@ -30,48 +26,48 @@
                     die('Database connection could not be established.');
                 }
             }
-            
+
             return self::$instance;
         }
-        
+
         public static function q($query) {
             if (func_num_args() == 1) {
                 return self::instance()->query($query);
             }
-            
+
             $args = func_get_args();
             return self::instance()->query(self::autoQuote(array_shift($args), $args));
         }
-        
+
         public static function x($query) {
             if (func_num_args() == 1) {
                 return self::instance()->exec($query);
             }
-            
+
             $args = func_get_args();
             return self::instance()->exec(self::autoQuote(array_shift($args), $args));
         }
-        
+
         public static function autoQuote($query, array $args) {
             $i = strlen($query);
             $c = count($args);
-            
+
             if (substr_count($query, '?') != $c) {
                 throw new UnexpectedValueException('Wrong parameter count: Number of placeholders and parameters does not match');
             }
-            
+
             while ($c--) {
                 while ($i-- && $query[$i] != '?');
-                
+
                 if (isset($query[$i+1]) && isset(self::$joiners[$query[$i+1]])) {
                     foreach ($args[$c] as $key => &$value) {
                         $value = '`' . $key . '`=' . self::instance()->quote($value);
                     }
-                    
+
                     $query = substr_replace($query, implode(self::$joiners[$query[$i+1]], $args[$c]), $i, 2);
                     continue;
                 }
-                
+
                 if (null === $args[$c]) {
                     $replace = 'NULL';
                 }
@@ -79,19 +75,19 @@
                     foreach ($args[$c] as &$value) {
                         $value = self::instance()->quote($value);
                     }
-                    
+
                     $replace = '(' . implode(',', $args[$c]) . ')';
                 }
                 else {
                     $replace = self::instance()->quote($args[$c]);
                 }
-                
+
                 $query = substr_replace($query, $replace, $i, 1);
             }
-            
+
             return $query;
         }
-        
+
         public static function __callStatic($method, $args) {
             return call_user_func_array(array(self::instance(), $method), $args);
         }
